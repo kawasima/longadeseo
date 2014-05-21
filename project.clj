87f0bab44fb5c04c0c94996c990f7c90b5673d74
kawasima <kawasima1016@gmail.com>
2014-05-21 13:31:18 +0900

    Implements SMB.

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :repositories [["alfresco" "https://maven.alfresco.com/nexus/content/repositories/releases"]
                  ["alfresco-thirdparty" "https://maven.alfresco.com/nexus/content/repositories/thirdparty"]
                  ["alfresco-public" "https://maven.alfresco.com/nexus/content/repositories/public"]]
-  :dependencies [[org.alfresco/alfresco-jlan-embed "4.2.f"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.alfresco/alfresco-jlan-embed "4.2.f"]]
+  :main longadeseo.core)
--- src/longadeseo/core.clj
+++ src/longadeseo/core.clj
@@ -1,29 +1,102 @@
 (ns longadeseo.core
-  (:import [org.alfresco.jlan.server.config ServerConfiguration]
+  (:import [org.alfresco.jlan.server.config ServerConfiguration CoreServerConfigSection]
            [org.alfresco.jlan.smb.server SMBServer CIFSConfigSection]
+           [org.alfresco.jlan.smb Dialect]
            [org.alfresco.jlan.netbios.server NetBIOSNameServer]
-           [org.alfresco.jlan.server.filesys FilesystemsConfigSection]
+           [org.alfresco.jlan.server.filesys FilesystemsConfigSection VolumeInfo SrvDiskInfo DiskSharedDevice]
+           [org.alfresco.jlan.server.filesys.cache StandaloneFileStateCache ]
+           [org.alfresco.jlan.server.auth CifsAuthenticator]
+           [org.alfresco.jlan.smb.server.disk JavaFileDiskDriver]
            [org.alfresco.jlan.debug Debug]
-           [java.net InetAddress]))
+           [org.springframework.extensions.config.element GenericConfigElement]
+           [java.net InetAddress]
+           [java.util Date]))
+
+(defn dialect-selector [cifs-config]
+  (let [dia-sel (.getEnabledDialects cifs-config)]
+    (.ClearAll dia-sel)
+    (doto dia-sel
+      (.AddDialect Dialect/DOSLanMan1)
+      (.AddDialect Dialect/DOSLanMan2)
+      (.AddDialect Dialect/LanMan1)
+      (.AddDialect Dialect/LanMan2)
+      (.AddDialect Dialect/LanMan2_1)
+      (.AddDialect Dialect/NT))))
 
 (def server-config (ServerConfiguration. "samba"))
 (def cifs-config (CIFSConfigSection. server-config))
 (doto cifs-config
+  (.setServerName "LONGADESEO")
+;  (.setDomainName "WORKGROUP")
+  (.setEnabledDialects (dialect-selector cifs-config))
   (.setSessionPort 1139)
   (.setNameServerPort 1137)
   (.setDatagramPort 1138)
-  (.setTcpipSMBPort 1445))
-(def filesys-config (FilesystemsConfigSection. server-config))
+  (.setTcpipSMB true)
+  (.setTcpipSMBPort 1445)
+  (.setHostAnnounceInterval 5)
+  (.setHostAnnouncer true)
+  (.setNetBIOSSMB true)
+  (.setNetBIOSDebug true)
+  (.setBroadcastMask "255.255.255.255")
+  (.setAuthenticator "org.alfresco.jlan.server.auth.EnterpriseCifsAuthenticator"
+    (GenericConfigElement. "Authenticator") CifsAuthenticator/USER_MODE true))
 
+(when (.hasSMBBindAddress cifs-config)
+  (.setNetBIOSBindAddress (.getSMBBindAddress cifs-config)))
+
+(doto (CoreServerConfigSection. server-config)
+  (.setThreadPool 25 50)
+  (.setMemoryPool
+    (into-array Integer/TYPE [256, 4096, 16384, 66000])
+    (into-array Integer/TYPE [20 20 5 5])
+    (into-array Integer/TYPE [100 50 50 50])))
+
+(defn disk-config []
+  (let [config (GenericConfigElement. "disk")
+        path-config (GenericConfigElement. "LocalPath")]
+    (.setValue path-config ".")
+    (.addChild config path-config)
+    config))
+
+(defn add-disk [filesys-config disk-name]
+  (let [driver (JavaFileDiskDriver.)
+        params (disk-config)
+        dev-ctx (.createContext driver disk-name params)
+        state-cache nil]
+    (doto dev-ctx
+      (.setConfigurationParameters params)
+      (.enableChangeHandler true)
+      (.setVolumeInformation (VolumeInfo. disk-name (mod (System/currentTimeMillis) Integer/MAX_VALUE) (Date. (System/currentTimeMillis))))
+      (.setDiskInformation (SrvDiskInfo. 2560000 64 512 2304000))
+      (.setShareName disk-name))
+    (when (and (.requiresStateCache dev-ctx) (nil? state-cache))
+      (let [state-cache (StandaloneFileStateCache.)]
+        (.addStateCache dev-ctx state-cache)))
+    (let [disk-dev (DiskSharedDevice. disk-name driver dev-ctx)]
+      (doto disk-dev
+        (.setComment "JLAN samba"))
+      (if (.hasStateCache dev-ctx)
+        (.addFileStateCache filesys-config name (.getStateCache dev-ctx))
+        (do
+          (.startFilesystem dev-ctx disk-dev)
+          (when (.hasStateCache dev-ctx)
+            (.. dev-ctx getStateCache (setDriverDetails disk-dev)))
+          (.addShare filesys-config disk-dev))))))
+ 
+(doto (FilesystemsConfigSection. server-config)
+  (add-disk "share"))
 
 (defn create-smb-server [server-config]
   (let [smb-server (SMBServer. server-config)]
     smb-server))
 
-(when (.hasNetBIOSSMB cifs-config)
-  (.addServer server-config (NetBIOSNameServer. server-config)))
+(defn -main [& args]
+  (. cifs-config getSessionPort)
+  (when (.hasNetBIOSSMB cifs-config)
+    (.addServer server-config (NetBIOSNameServer. server-config)))
+  (.addServer server-config (create-smb-server server-config))
+  (dotimes [i (.numberOfServers server-config)]
+    (.startServer (.getServer server-config i))))
 
-(. cifs-config getSessionPort)
-(.addServer server-config (create-smb-server server-config))
-(..  (.getServer server-config 0) getFilesystemConfiguration)
-(.startServer (.getServer server-config 0))
+
